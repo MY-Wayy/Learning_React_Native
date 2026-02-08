3c0b6ec57c66fcfffac63dc9d9ad646a587d7c0c
MY-Wayy <rlfalsgur316@gmail.com>
2026-02-09 00:17:42 +0900

    ch2.4 까지 수강 (2026.2.8)

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello!</Text>
-      <StatusBar style="light" />
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 3, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 2, backgroundColor: "orange" }}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000000",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 28,
-    color: "white",
-  },
-});
